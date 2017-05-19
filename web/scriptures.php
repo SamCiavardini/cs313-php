da864f9dc40d2c16f76da58862ba0d2e173ae6f0
SamCiavardini <sam.ciavardini@gmail.com>
2017-05-19 16:28:38 -0600

    new test file

---
+++ web/scriptures.php
@@ -0,0 +1,34 @@
+<?php
+$pass="Inthenavy15"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Scriptures!</title>
+</head>
+
+<body>
+<?php
+	try{
+
+		$db = new PDO("pgsql:host=localhost;port=5432;dbname=scriptures;", "postgresql-concentric-32341", $pass);
+
+
+	}
+	catch(PDOException $ex){
+		echo "error: " . $ex->getMessage();
+	}
+	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+	{
+  		echo $row['book'] . " " 
+  		. $row['chapter'] .":"
+  		. $row['verse'] . " - " 
+  		. $row['content'];
+  		
+  		echo '<br/>';
+	}
+?>
+	
+</body>
