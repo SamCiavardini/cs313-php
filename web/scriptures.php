c7ed776e65fe930f54a37b2a2c1d4cb4e6cff01e
SamCiavardini <sam.ciavardini@gmail.com>
2017-05-19 16:45:50 -0600

    corrected db info

--- web/scriptures.php
+++ web/scriptures.php
@@ -13,7 +13,7 @@ $pass="53247736113d300994a1897a4ea11b45fef4df4028ca7947d16ce9bc97ac8e2b"
 <?php
 	try{
 
-		$db = new PDO("pgsql:host=ec2-54-221-255-153.compute-1.amazonaws.com;dbname=scriptures;", "postgresql-concentric-32341", $pass);
+		$db = new PDO("pgsql:host=ec2-54-221-255-153.compute-1.amazonaws.com;dbname=scriptures;", "ghgozncuniefut", $pass);
 
 
 	}
