83febd9231dc1ec0cb1caa557cf7bb980b111b4c
SamCiavardini <sam.ciavardini@gmail.com>
2017-05-22 13:38:57 -0600

    content now displays on viewmedia

--- web/viewmedia.php
+++ web/viewmedia.php
@@ -33,22 +33,24 @@ $pass="53247736113d300994a1897a4ea11b45fef4df4028ca7947d16ce9bc97ac8e2b"
 	catch(PDOException $ex){
 		echo "error: " . $ex->getMessage();
 	}
+
 	foreach ($db->query("SELECT * FROM post WHERE id = ". $id) as $thepost){
   		echo "<h1>" . $thepost[title] . "</h1><br><img src='" . $thepost[thumbnail] . "'/><h2>" 
-  		. $thepost[subtitle] . "</h2><br>" . $thepost[content];
-  		
-  		
-  		echo '<br/>';
+  		. $thepost[subtitle] . "</h2><br/>";
 	}?>
-	<h3>Topics:</h3>
-	<?php
-	
 
+	<h3>Topics:</h3>
+
+	<?php
 	foreach ($db->query("SELECT * FROM posttopic WHERE post_id = ". $id) as $posttopic){
 		foreach ($db->query("SELECT topicname FROM topic WHERE id = ". $posttopic[topic_id]) as $topicName){
 			echo "<strong>" . $topicName[topicname] . ", </strong>";
 		}
-	}?>
+	}
+	foreach ($db->query("SELECT content FROM post WHERE post_id = " . $id) as $post){
+		echo $post[content];
+	}
+	?>
 	<h1>Comments:</h1>
 	<?php
 	foreach ($db->query("SELECT * FROM comment WHERE post_id = ". $id) as $comment){
